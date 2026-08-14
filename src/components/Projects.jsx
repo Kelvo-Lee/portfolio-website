ba2068894f4d9181add701dcbafa119ee1da0fe1
Kelvo-Lee <akharamenkelvin@gmail.com>
2026-08-14 03:02:09 +0100

    fix: update project3 image extension casing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container, Row, Col, Card, Badge, Button } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import projectImage3 from '../assets/project3.JPG';
+import projectImage3 from '../assets/project3.jpg';
 import projectImage4 from '../assets/project4.png';
 import projectImage5 from '../assets/project5.png';
 import projectImage6 from '../assets/project6.png';
